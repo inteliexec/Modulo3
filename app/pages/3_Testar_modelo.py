e121a6b3fa1117fe018ef84cef9c17f13ad77466
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2024-09-03 14:48:12 -0300

    feat(app): :sparkles: Add Google Sheets integration to Streamlit app

--- app/pages/3_Testar_modelo.py
+++ app/pages/3_Testar_modelo.py
@@ -2,20 +2,17 @@ import streamlit as st
 import pandas as pd
 import io
 import boto3
+from streamlit_gsheets import GSheetsConnection
 
 from config.config import set_page_config
 set_page_config()
 
-st.markdown(
-                """
-                <style>
-                [data-testid="stElementToolbar"] {
-                    display: none;
-                }
-                </style>
-                """,
-                unsafe_allow_html=True
-            )
+url = "https://docs.google.com/spreadsheets/d/1yVbOkuSSZ_oH7OKicOGclJHTKjORkoKt03jIFaEJCHk/edit?usp=sharing"
+
+def sheets_connection():
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    data = conn.read(spreadsheet=url)
+    return data
 
 def to_excel(df):
         output = io.BytesIO()
@@ -25,7 +22,7 @@ def to_excel(df):
         processed_data = output.getvalue()
         return processed_data
 
-def upload_file_to_s3(df, company_name, file_name):
+def upload_file_to_s3(df, company, file_name):
     excel_buffer = io.BytesIO()
     df.to_excel(excel_buffer, index=False, engine='openpyxl')
     excel_buffer.seek(0)
@@ -33,7 +30,7 @@ def upload_file_to_s3(df, company_name, file_name):
     s3_client.upload_fileobj(
         excel_buffer,
         Bucket='inteli-exec-dev',
-        Key=f'{company_name}/{file_name}.xlsx'
+        Key=f'{company}/{file_name}.xlsx'
     )
     st.write("O upload do arquivo foi realizado com sucesso!")
     
@@ -43,6 +40,7 @@ if  st.session_state.get('is_fit') is None:
 else:
     if st.session_state['is_fit']:
         st.write("# Aplicar score na base de teste")
+        data = sheets_connection()
         val = st.file_uploader("Envie seu arquivo para teste", type=['xlsx'], accept_multiple_files=False)
         if val is not None:
             model = st.session_state['model']
@@ -51,10 +49,13 @@ else:
             df_pred = X_val.copy()
             df_pred['Prediction'] = model.predict_proba(X_val)[:,1]
             st.dataframe(df_pred.head(30))
-            company_name = st.selectbox("Qual o nome da sua empresa?", ["BTG", "Ambev", "Outra"], index=None)
-            file_name = st.text_input("Insira o nome que deseja fornecer ao arquivo", "")
-            if file_name != "" and company_name != None:
-                upload_button = st.button("Realizar upload")
-                if upload_button:
-                    upload_file_to_s3(df_pred, company_name, file_name)
+            company = st.selectbox("Qual o nome da sua empresa?", data.columns, index=None)
+            if company != None:
+                row = data[company].unique()
+                name = st.selectbox("Qual o seu nome?", pd.Series(row).dropna().values, index=None)
+                file_name = st.text_input("Insira o nome que deseja fornecer ao arquivo", "")
+                if file_name != "" and name != None:
+                    upload_button = st.button("Realizar upload")
+                    if upload_button:
+                        upload_file_to_s3(df_pred, company, file_name)
 
