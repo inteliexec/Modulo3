5da43b26afe8b50b2882f64959901afc7254ba77
Pablo Ruan <99209107+Pablo-RLV@users.noreply.github.com>
2024-10-28 17:50:30 -0300

    Update userdata.bash

--- aws/userdata.bash
+++ aws/userdata.bash
@@ -4,9 +4,11 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update && sudo apt install -y docker-ce
-sudo usermod -aG docker ${USER}
+sudo usermod -aG docker ubuntu
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
+cd /home/ubuntu
 git clone https://github.com/inteliexec/Modulo3.git
-cd ~/Modulo3
-sudo docker compose up
+cd Modulo3
+sudo docker compose up -d
